36e2e829da1373a16a39f4fc2ff9e56ac3605abc
aali4430 <aali4430@gmail.com>
2019-01-02 12:40:46 -0800

    Add files via upload

---
+++ Compression.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace client
+{
+    class Compression
+    {
+        public static string Compress(string filePath, string destPath, string fileName)
+        {
+            byte[] raw = File.ReadAllBytes(filePath);
+            byte[] zipped = null;
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (GZipStream gzip = new GZipStream(memory,
+                    CompressionMode.Compress, true))
+                {
+                    gzip.Write(raw, 0, raw.Length);
+                }
+                zipped = memory.ToArray();
+            }
+            // Write compressed data.
+            string directory = Path.Combine(destPath, Path.GetFileNameWithoutExtension(fileName) + ".gz");
+            File.WriteAllBytes(directory, zipped);
+            File.Delete(filePath);
+            return directory;
+        }
+        public static string Decompress(string filePath, string destPath, string fileName)
+        {
+            byte[] gzip = File.ReadAllBytes(filePath);
+            byte[] unzip = null;
+            // Create a GZIP stream with decompression mode.
+            // ... Then create a buffer and write into while reading from the GZIP stream.
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip),
+                CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    unzip = memory.ToArray();
+                }
+            }
+            string directory = Path.Combine(destPath, fileName);
+            File.WriteAllBytes(directory, unzip);
+            File.Delete(filePath);
+            return directory;
+
+        }
+    }
+}
---
+++ client.cs
@@ -0,0 +1,37 @@
+﻿//----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//----------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace Microsoft.Samples.Mtom
+{
+    //The service contract is defined in generatedClient.cs, generated from the service by the svcutil tool.
+
+    //Client implementation code.
+    class Client
+    {
+        
+        static void Main()
+        {
+            string pickup = @"C:\NewFile\PickUp\";
+            
+            string queue = @"C:\NewFile\FileQueue\";
+            string delive = @"C:\NewFile\Deliver\";
+            string[] files = Directory.GetFiles(pickup, "*.*", SearchOption.AllDirectories);
+            foreach (var fil in files) {
+                string extention = Path.GetFileName(fil);
+                string outp = client.Compression.Compress(fil, queue, extention);
+                client.Compression.Decompress(outp, delive, extention);
+            }
+           
+        }
+        
+
+
+    }
+}
